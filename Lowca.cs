f986c96fdeda1a4ff78999413303845c3c0cf4db
Dawid Raj <dwdraj8@gmail.com>
2021-10-29 22:09:05 +0200

    Dodano zarysy klas pochodnych klasy klasaPostaci: Mag i Lowca

---
+++ Lowca.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleRPGv22
+{
+    class Lowca : klasaPostaci
+    {
+        public Lowca()
+        {
+
+        }
+
+        public Lowca(string name)
+        {
+            CharName = name;
+
+            CharHp = 400;
+
+            CharMp = 200;
+
+            CharLvl = 1;
+
+            CharLp = 1;
+
+            CharStr = 7;
+
+            CharInt = 4;
+
+            CharDex = 10;
+
+            SkillAvailable = false;
+
+            SkillLearned = false;
+        }
+
+    }
+}
---
+++ Mag.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleRPGv22
+{
+    class Mag : klasaPostaci
+    {
+        public Mag()
+        {
+
+        }
+
+        public Mag(string name)
+        {
+            CharName = name;
+
+            CharHp = 250;
+
+            CharMp = 350;
+
+            CharLvl = 1;
+
+            CharLp = 1;
+
+            CharStr = 4;
+
+            CharInt = 10;
+
+            CharDex = 7;
+
+            SkillAvailable = false;
+
+            SkillLearned = false;
+        }
+    }
+}
